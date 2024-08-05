83770e77d1765de9cb24304c5e14e0f6e941e708
okanolgun <98118524+okanolgun@users.noreply.github.com>
2024-08-05 13:59:20 +0300

    55JumpGame.java

---
+++ 55JumpGame.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int target = nums.length - 1 ; 
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if(target <= i + nums[i]) {
+                target = i;
+            }
+        }
+
+        return target == 0 ; 
+    }
+}
